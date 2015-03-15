0cfc10ecf31a72a3957fb8ff94c250f4bc4be321
Pawel Piotrowski <pawel@flytype.org>
2015-03-15 19:28:41 +0000

    og image added and grunt dont rev images

--- Gruntfile.js
+++ Gruntfile.js
@@ -186,9 +186,9 @@ module.exports = function (grunt) {
 				files: {
 					src: [
 						'<%= yeoman.dist %>/scripts/{,*/}*.js',
-						'<%= yeoman.dist %>/styles/{,*/}*.css',
-						'<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'/*,
-						'<%= yeoman.dist %>/fonts/*' */
+						'<%= yeoman.dist %>/styles/{,*/}*.css'//,
+						//'<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+						//'<%= yeoman.dist %>/fonts/*'
 					]
 				}
 			}
